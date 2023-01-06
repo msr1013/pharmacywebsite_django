b116e3c2cfd67fee16047cd63344b6d831ce50c6
msr1013 <rawalsinghmanish@gmail.com>
2023-01-06 08:20:28 +0545

    username email validation while registering

--- pharmacywebsite/authentication/views.py
+++ pharmacywebsite/authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
 from django.contrib import auth
@@ -10,6 +10,15 @@ class LoginView(View):
     def get(self, request):
         return render(request, 'authentication/login.html')
 
+    def post(self, request):
+        req_username = request.POST.get('username')
+        req_password = request.POST.get('password')
+
+        if req_username and req_password:
+            auth.authenticate(username=req_username, password=req_password)
+            return render(request, 'medicines/index.html')
+        return redirect("login")
+
 
 class RegisterView(View):
     def get(self, request):
@@ -17,10 +26,11 @@ class RegisterView(View):
         return render(request, 'authentication/register.html')
 
     def post(self, request):
-        req_email = request.POST.get('email'),
-        req_password = request.POST.get('password'),
-        req_username = request.POST.get('username'),
+        req_email = request.POST.get('email')
+        req_password = request.POST.get('password')
+        req_username = request.POST.get('username')
         user = User.objects.create_user(email=req_email, username=req_username)
         user.set_password(req_password)
+        user.is_active = False
         user.save()
         return render(request, 'authentication/register.html')
--- pharmacywebsite/medicines/views.py
+++ pharmacywebsite/medicines/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required(login_url='/authentication/login')
+def medicine_index(request):
+    return render(request, 'medicines/index.html')
