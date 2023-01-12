78dfcb647d827a493ce6af1275b62ec046b75b4b
msr1013 <rawalsinghmanish@gmail.com>
2023-01-12 08:44:00 +0545

    edit show and delete medicine

--- pharmacywebsite/medicines/urls.py
+++ pharmacywebsite/medicines/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('medicines/', views.medicine_index, name='medicines'),
     path('add-medicine/', views.medicine_create, name='add-medicine'),
+    path('edit_medicine/<int:id>', views.medicine_edit, name='edit_medicine'),
+    path('show_medicine/<int:id>', views.medicine_show, name='show_medicine'),
+    path('delete_medicine/<int:id>', views.medicine_delete, name='delete_medicine'),
 
     # categories
     path('add-category/', views.category_create, name='add-category'),
