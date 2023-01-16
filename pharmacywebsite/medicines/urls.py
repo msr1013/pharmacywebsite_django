2a566e8624df0ffd838c69dff2e07511accf6f6f
msr1013 <rawalsinghmanish@gmail.com>
2023-01-16 08:35:47 +0545

    add edit,show,delete functionality

--- pharmacywebsite/medicines/urls.py
+++ pharmacywebsite/medicines/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('medicines/', views.medicine_index, name='medicines'),
     path('add-medicine/', views.medicine_create, name='add-medicine'),
-    path('edit_medicine/<int:id>', views.medicine_edit, name='edit_medicine'),
-    path('show_medicine/<int:id>', views.medicine_show, name='show_medicine'),
-    path('delete_medicine/<int:id>', views.medicine_delete, name='delete_medicine'),
+    path('update-medicine/', views.medicine_update, name='update-medicine'),
+    path('edit_medicine/<int:id>', views.medicine_edit, name='edit-medicine'),
+    path('show_medicine/<int:id>', views.medicine_show, name='show-medicine'),
+    path('delete_medicine/<int:id>', views.medicine_delete, name='delete-medicine'),
 
     # categories
     path('add-category/', views.category_create, name='add-category'),
--- pharmacywebsite/medicines/views.py
+++ pharmacywebsite/medicines/views.py
@@ -31,9 +31,25 @@ def medicine_index(request):
 
 @login_required(login_url='/authentication/login')
 def medicine_edit(request, id):
-    data = Medicine.objects.get(id-id)
+    data = Medicine.objects.get(id=id)
     context = {"data": data}
     return render(request, "medicines/edit_medicine.html", context)
+
+
+@login_required(login_url='/authentication/login')
+def medicine_update(request):
+    if request.method == "POST":
+        data = Medicine.objects.get(id=request.POST.get('id'))
+        data.name = request.POST.get('name')
+        data.description = request.POST.get('description')
+        data.expiry_date = request.POST.get('expiry_date')
+        data.mg = request.POST.get('mg')
+        data.price = request.POST.get('price')
+        data.quantity = request.POST.get('quantity')
+        data.save()
+        messages.success(request, 'Updated successfully')
+        return redirect('medicines')
+    return redirect('medicines')
 
 
 @login_required(login_url='/authentication/login')
@@ -48,7 +64,7 @@ def medicine_delete(request, id):
     data = Medicine.objects.get(id=id)
     data.delete()
     messages.success(request, 'Deleted successfully')
-    return render(request, "medicines/edit_medicines.html")
+    return redirect("medicines")
 
 
 @login_required(login_url='/authentication/login')
